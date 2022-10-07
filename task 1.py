ddf579cfa5eef1d2d1cbd8a0488b296b343167f0
Elizabeth <i_am_exlusive@mail.ru>
2022-10-07 19:55:13 +0300

    PY111 exam

---
+++ task 1.py
@@ -0,0 +1,10 @@
+a = len(arr) - 1  # O(1)
+out = list()  #
+while a > 0: # O(log(n))
+    out.append(arr[a]) # O(1)
+    a = a // 1.7 # O(1)
+out.merge_sort() # O(n)
+
+print()
+
+
---
+++ task 3.py
@@ -0,0 +1,44 @@
+"""Сорт
+Дано: массив из 10**6 целых чисел, каждое из которых лежит на отрезке [13, 25].
+Задача: отсортировать массив наиболее эффективным способом """
+import random
+import time
+
+
+def sort_1(array: list) -> list:
+    """ Bubble sort function"""
+
+    for i in range(len(array)):
+        for j in range(len(array) - 1):
+            if array[j] > array[j + 1]:
+                array[j + 1], array[j] = array[j], array[j + 1]
+
+    return array
+
+
+def sort_2(array: list) -> list:
+    """ Quick sort function """
+    if len(array) <= 1:
+        return array
+    num_random = random.choice(array)
+    left_side = [num for num in array if num < num_random]
+    equals = [num_random] * array.count(num_random)
+
+    right_side = [num for num in array if num > num_random]
+    return sort_2(left_side) + equals + sort_2(right_side)
+
+
+arr = [random.randint(13, 27) for _ in range(10**6 + 1)]
+
+if __name__ == '__main__':
+    time_list_1 = []
+    time_list_2 = []
+    for i in range(11):
+        start = time.perf_counter()
+        sort_2(arr)
+        time_list_2.append(time.perf_counter() - start)
+        sort_1(arr)
+        time_list_1.append(time.perf_counter() - start)
+
+    print(f"Bubble sort time: {sum(time_list_1) / 10}, Quick sort time: {sum(time_list_2) / 10}")
+    #
