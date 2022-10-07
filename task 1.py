948364a24b4353a2493817e7c8c8d349bb61204d
Elizabeth <i_am_exlusive@mail.ru>
2022-10-07 20:13:27 +0300

    task 1

--- task 1.py
+++ task 1.py
@@ -1,10 +1,11 @@
 a = len(arr) - 1  # O(1)
-out = list()  #
+out = list()  # O(1)
 while a > 0: # O(log(n))
     out.append(arr[a]) # O(1)
     a = a // 1.7 # O(1)
 out.merge_sort() # O(n)
 
-print()
+print('O(log(n))')
+
 
 
